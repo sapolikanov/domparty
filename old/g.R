9fadc5e8288b88002eef8bd7907d1360400de159
sapolikanov <91873604+sapolikanov@users.noreply.github.com>
2024-08-08 08:30:17 -0400

    Data building, continued
    Additionally removed out-of-place scripts and data, updated renv lock accordingly

--- old/g.R
+++
@@ -1,6 +0,0 @@
-library(foreign)
-library(nnet)
-library(ggplot2)
-library(reshape2)
-
-new.env()
--- utilities/check_packages.R
+++ utilities/check_packages.R
@@ -15,8 +15,8 @@ packages = c(
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
-    renv::install(x, dependencies = "all")
-    library(x)
+    install.packages(x, dependencies = TRUE)
+    library(x, character.only = TRUE)
   }
 })
 
